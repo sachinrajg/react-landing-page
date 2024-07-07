3c31ca7b6551d0e0d46b62ab2a5e173b6a6cdd21
Dharani-200 <sachinmcin@gmail.com>
2024-07-07 12:39:18 +0530

    App.js Components Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import NavBar from './components/NavBar';
-import CarouselComponent from './components/CarouselComponents';
 import LandingPage from './components/LandingPage';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
@@ -11,13 +8,8 @@ const App = () => (
   <Router>
     
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<Footer />} />
-      <Route path="/contact" element={<Footer/>} />
       <Route path="/menu" element={<Menu />} />
     </Routes>
-    <NavBar/>
-    <CarouselComponent/>
     <LandingPage/>
     <Footer/>
   </Router>
